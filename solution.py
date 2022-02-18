80c6c457eee6796bb48394839c01bae1b4a6eaef
shaunbartole <82050408+shaunbartole@users.noreply.github.com>
2022-02-18 10:15:08 -0500

    commit 2

--- solution.py
+++ solution.py
@@ -8,14 +8,17 @@ def webServer(port=13331):
   serverSocket = socket(AF_INET, SOCK_STREAM)
   #Prepare a server socket
   serverSocket.bind(("", port))
-  #Fill in start
-
-  #Fill in end
+  serverSocket.listen(1)
+  print("The server is ready to receive")
 
   while True:
     #Establish the connection
     #print('Ready to serve...')
-    connectionSocket, addr = #Fill in start      #Fill in end
+    connectionSocket, addr = serverSocket.accept()
+    sentence = connectionSocket.recv(1024).decode()
+    capitalizedSentence = sentence.upper()
+    connectionSocket.send(capitalizedSentence.encode())
+    connectionSocket.close()
     try:
 
       try:
